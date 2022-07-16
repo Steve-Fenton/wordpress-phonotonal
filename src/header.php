d6ed5a1a831e1ddabc18f3809244e94bb1749098
Steve Fenton <github@stevefenton.co.uk>
2022-07-16 15:01:47 +0100

    Improved titles

--- src/header.php
+++ src/header.php
@@ -9,6 +9,15 @@ function wp_get_current_url() {
 	return home_url($_SERVER['REQUEST_URI']);
 }
 
+function cat_sort_reverse($a, $b) {
+	if ($a->parent == $b->parent) {
+		return 0;
+	}
+	return ($a->parent > $b->parent) ? -1 : 1;
+}
+
+$title_parts = array();
+
 $title = wp_title('', false);
 if (strlen($title) == 0) {
 	$title = 'Home';
@@ -25,19 +34,40 @@ if ($qry) {
 
 	switch ($qry_type) {
 		case 'WP_Post':
+			$post_title = get_the_title($qry);
+			$parts = explode('&#8211;', $post_title);
+			
+			if (count($parts) > 1) {
+				$title_parts[] = trim($parts[1]);
+			}
+			$title_parts[] = trim($parts[0]);
+
+			$post_category = get_the_category($qry->ID);
+			uasort($post_category, 'cat_sort_reverse');
+			foreach($post_category as $post_cat) {
+				$title_parts[] = $post_cat->name;
+			}
+
 			if (strlen($meta_description) == 0) {
 				$meta_description = wp_trim_words(get_the_excerpt($qry), 20);
 			}
 		
 			if (strlen($meta_keywords) == 0) {
-				$post_title = get_the_title($qry);
-				$parts = explode('&#8211;', $post_title);
 				$meta_keywords = strtolower(implode(',', $parts));
 			}
 
 			$author_name = get_the_author_meta('first_name', $qry->post_author) . ' ' . get_the_author_meta('last_name', $qry->post_author);
 			break;
 		case 'WP_Term':
+			$title_parts[] = $qry->name;
+
+			if ($qry->taxonomy == 'category') {
+				$parentCatName = get_cat_name($qry->parent);
+				if ($parentCatName != null) {
+					$title_parts[] = $parentCatName;
+				}
+			}
+
 			if (strlen($meta_description) == 0) {
 				$meta_description = wp_trim_words($qry->description, 20);
 			}
@@ -47,22 +77,27 @@ if ($qry) {
 			}
 			break;
 		case 'WP_User':
+			$user_display_name = get_the_author_meta('display_name', $qry->ID);
+			$title_parts[] = $user_display_name;
+
 			if (strlen($meta_description) == 0) {
 				$meta_description = wp_trim_words($qry->user_description, 20);
 			}
 		
 			if (strlen($meta_keywords) == 0) {
-				$author_name = get_the_author_meta('display_name', $qry->ID);
+				$author_name = $user_display_name;
 				$meta_keywords = strtolower($author_name);
 			}
 			break;
 	}
+
+	$title_parts[] = get_bloginfo('name');
 }
 ?><!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head <?php language_attributes(); ?>>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<title><?php echo $title ?> | <?php bloginfo('name') ?></title>
+	<title><?php echo preg_replace('/\s+/', ' ', implode(' | ', $title_parts)) ?></title>
 	<meta name="description" content="<?php
 	if ($qry) {
         echo str_replace('"', '', $meta_description);
